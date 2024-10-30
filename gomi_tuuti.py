df92aee1a7c68387685d6ef1a350cb4850acb052
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-10-30 12:59:56 +0900

    とりあえず、今日の日付からその日のごみ収集内容を取り出すことができました。ただし、10月のみなので、月が変わっても対応できるよう修正必須。

--- gomi_tuuti.py
+++ gomi_tuuti.py
@@ -1,4 +1,5 @@
-import requests
+import requests # Webサイトにデータを送るようリクエストする
+import datetime # 日付を取得するためのライブラリ
 import os # OSに関連する機能を提供する。ここでは環境変数にアクセスするための機能。
 from dotenv import load_dotenv # .envファイルの内容を環境変数として読み込む
 from bs4 import BeautifulSoup
@@ -11,4 +12,14 @@ load_url = os.getenv("LOAD_URL")
 html = requests.get(load_url)
 soup = BeautifulSoup(html.content, "html.parser")
 
-print(soup)
+# 今日の日付を取得
+today = datetime.date.today()
+
+# 今日の日付のタグを取り出す
+td_class = "td" + str(today.day)
+date = soup.find("td", class_=td_class)
+
+# その日のごみ収集内容を取り出す
+trash_kind = date.find("span", class_="trash_kind_name")
+
+print(trash_kind.text)
